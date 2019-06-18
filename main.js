f7256448506c9730bd0669446791b5695c05964c
Veronica Johnson <ronijohnveronica@gmail.com>
2019-06-18 16:01:08 -0400

    second

--- main.js
+++ main.js
@@ -14,13 +14,14 @@
         }
         for(var k=0; k<=100; k++){
             console.log(k);
-             if(k % 5 === 0) {
-                console.log(b);
-            }
-            if(k % 3 === 0) {
-                console.log(f);
-                }
             if((k % 3 === 0) && (k % 5 === 0)) {
                 console.log(f+b);
-            }   
+            }
+            else if(k % 5 === 0) {
+                console.log(b);
+            }
+            else if(k % 3 === 0) {
+                console.log(f);
+                }
+               
         } 
